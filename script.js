97ef7ff0c6d6d14e4ce3b71f4129f4155d7b6ed8
mluzzi <muriloluzzi@gmail.com>
2022-06-07 21:24:11 -0300

    finished html and css

---
+++ script.js
@@ -0,0 +1,24 @@
+const ac = document.querySelector('.ac');
+const plusMinus = document.querySelector('.plus-minus');
+const percentage = document.querySelector('.percentage');
+const divide = document.querySelector('.divide');
+const multiply = document.querySelector('.multiply');
+const minus = document.querySelector('.minus');
+const plus = document.querySelector('.plus');
+const equal = document.querySelector('.equal');
+const seven = document.querySelector('.seven');
+const eigth = document.querySelector('.eigth');
+const nine = document.querySelector('.nine');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const zero = document.querySelector('.zero');
+const dot = document.querySelector('.dot');
+
+console.log(ac);
+ac.addEventListener('click', function () {
+  console.log('clicked');
+});
